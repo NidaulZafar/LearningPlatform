76c30e641099c31212fc1853e15f86f45422f1a8
Zafar <zafar@settly.nl>
2024-01-13 17:30:07 +0100

    creating views

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,7 +1,26 @@
 import {createBrowserRouter} from "react-router-dom";
+import Login from "./views/Login.jsx";
+import Signup from "./views/Signup.jsx";
+import Dashboard from "./views/Dashboard.jsx";
+import NotFound from "./views/NotFound.jsx";
 
 const router = createBrowserRouter([
-
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/signup",
+    element: <Signup/>
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard/>
+  },
+  {
+    path: "*",
+    element: <NotFound/>
+  }
 ])
 
 
