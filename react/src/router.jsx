3f19c4bee97827bd71bc4c7f667d6abfa50eb4d9
Zafar <zafar@settly.nl>
2024-01-13 17:58:13 +0100

    creating context

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,21 +1,39 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom";
 import Login from "./views/Login.jsx";
 import Signup from "./views/Signup.jsx";
 import Dashboard from "./views/Dashboard.jsx";
 import NotFound from "./views/NotFound.jsx";
+import DefaultLayout from "./components/DefaultLayout.jsx";
+import GuestLayout from "./components/GuestLayout.jsx";
 
 const router = createBrowserRouter([
   {
-    path: "/login",
-    element: <Login/>
+    path: "/",
+    element: <DefaultLayout/>,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/dashboard"/>
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard/>
+      }
+    ]
   },
   {
-    path: "/signup",
-    element: <Signup/>
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard/>
+    path: "/",
+    element: <GuestLayout/>,
+    children: [
+      {
+        path: "/login",
+        element: <Login/>
+      },
+      {
+        path: "/signup",
+        element: <Signup/>
+      },
+    ]
   },
   {
     path: "*",
