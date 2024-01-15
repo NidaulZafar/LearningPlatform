a0cd8d2dadbb4403edcd202dbbba5f5443eadcd7
N Zafar <n_zafar_007@hotmail.com>
2024-01-15 13:43:49 +0100

    Signup request

---
+++ app/Http/Requests/SignupRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class SignupRequest extends FormRequest
+{
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'type' => ['required', Rule::in(['instructor', 'student'])],
+            'email' => ['required', 'email', Rule::unique($this->type === 'instructor' ? 'instructors' : 'students', 'email')],
+            'password' => ['required', 'string', 'min:3', 'confirmed'],
+        ];
+    }
+}
