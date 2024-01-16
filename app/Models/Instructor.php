e4b4f998a7e1b3a5c6fc1a1108582da34d83e680
N Zafar <n_zafar_007@hotmail.com>
2024-01-16 16:49:59 +0100

    Instructor model update

--- app/Models/Instructor.php
+++ app/Models/Instructor.php
@@ -5,10 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Instructor extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'type'
+    ];
     public function courses(): HasMany
     {
         return $this->hasMany(Course::class);
