42399e76e850553f5667508fe77d524dc7b235ee
N Zafar <n_zafar_007@hotmail.com>
2024-01-16 16:51:27 +0100

    Student model updated

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,12 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Student extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
-    // Student model
+
+protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'type'
+    ];
+
+
     public function courses()
     {
         return $this->belongsToMany(Course::class);
