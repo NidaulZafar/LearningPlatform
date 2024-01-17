a2d088ee656ab0e17333ca2364ebb7703fe0f09e
N Zafar <n_zafar_007@hotmail.com>
2024-01-17 18:52:20 +0100

    404 layout updated

--- react/src/views/NotFound.jsx
+++ react/src/views/NotFound.jsx
@@ -4,6 +4,7 @@ export default function NotFound() {
       404 - Page Not Found
       Are you Lost?
       Let's go back to the <a href="/">Home Page</a>
+      Or maybe you want to <a href="/login">Login</a>?
     </div>
   )
 }
