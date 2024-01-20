4a78b1c4cb4e8b728cbd46d1001aed70f29207bf
N Zafar <n_zafar_007@hotmail.com>
2024-01-20 21:08:20 +0100

    dashboard update

--- react/src/views/Dashboard.jsx
+++ react/src/views/Dashboard.jsx
@@ -8,7 +8,6 @@ export default function Dashboard() {
             Dashboard
           Welcome {user.name}
           You are logged in as {user.type}
-        {/*  create a logout button */}
           <a href="/logout">Logout</a>
 
         </div>
