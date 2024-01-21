d1253701af98a813f444facb6b80e249bf2d493e
N Zafar <n_zafar_007@hotmail.com>
2024-01-21 15:10:16 +0100

    Axios client refactor

--- react/src/axios-client.js
+++ react/src/axios-client.js
@@ -1,7 +1,5 @@
-
 // axios-client.js
 import axios from 'axios';
-
 
 
 const axiosClient = axios.create({
@@ -24,7 +22,6 @@ axiosClient.interceptors.response.use((response) => {
   return response;
 }, (error) => {
   try {
-    console.log('try error', error);
     const {response} = error;
     if (response.status === 401) {
       localStorage.removeItem('ACCESS_TOKEN');
