0ad4152aea88711bcd0c868628181045c9b571d1
N Zafar <n_zafar_007@hotmail.com>
2024-01-21 14:53:38 +0100

    Announcements page refactor

--- react/src/views/Announcements.jsx
+++ react/src/views/Announcements.jsx
@@ -1,26 +1,25 @@
-export default function Announcements(){
+export default function Announcements() {
 //   import announcements from announcements table
   const announcements = [
-    {id:1, title:"Announcement 1", body:"This is the first announcement"},
-    {id:2, title:"Announcement 2", body:"This is the second announcement"},
-    {id:3, title:"Announcement 3", body:"This is the third announcement"}
+    {id: 1, title: "Announcement 1", body: "This is the first announcement"},
+    {id: 2, title: "Announcement 2", body: "This is the second announcement"},
+    {id: 3, title: "Announcement 3", body: "This is the third announcement"}
   ]
-return(
-<>
-Announcements
-<ul>
-{announcements.map((announcement) => {
-  return(
-    <li key={announcement.id}>
-      <h3>{announcement.title}</h3>
-      <p>{announcement.body}</p>
-    </li>
+  return (
+    <>
+      Announcements
+      <ul>
+        {announcements.map((announcement) => {
+          return (
+            <li key={announcement.id}>
+              <h3>{announcement.title}</h3>
+              <p>{announcement.body}</p>
+            </li>
+          )
+        })}
+      </ul>
+
+
+    </>
   )
-})}
-</ul>
-
-
-
-</>
-)
 }
