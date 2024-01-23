3fdf3f19fc0e7940dbaaaec0847e8db543a3289c
N Zafar <n_zafar_007@hotmail.com>
2024-01-23 09:57:40 +0100

    user token fixed

--- react/src/views/Dashboard.jsx
+++ react/src/views/Dashboard.jsx
@@ -5,24 +5,15 @@ import {useNavigate} from "react-router-dom";
 export default function Dashboard() {
     const navigate = useNavigate();
     const {user,setUser, setToken} = useStateContext();
-
-    const logout = async () => {
-      try{
-        await axiosClient.post('/logout');
-        setUser(null);
-        setToken(null);
-        navigate('/login');
-      } catch (error) {
-        console.log("logout error", error);
+    if (!user) {
+        navigate('/login')
     }
-      }
 
     return (
         <div>
             Dashboard
           Welcome {user.name}
           You are logged in as {user.type}
-          <button onClick={logout}>Logout</button>
         </div>
     )
 }
