899affcdb8b083dd5e597bb741d36b2a4c20a8a4
N Zafar <n_zafar_007@hotmail.com>
2024-01-26 10:24:59 +0100

    styles

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -42,94 +42,66 @@ export default function Signup() {
       })
   }
 
-  return (<div className="container">
-    <div className="row justify-content-center">
-      <div className="col-md-8">
-        <div className="card">
-          <h2 className="">Signup for a new account</h2>
+  return (
+    <div className="page-content">
+      <div className="form-v6-content">
+        <div className="form-left">
+          <img src="https://picsum.photos/seed/picsum/200/300" alt="form"/>
+        </div>
+        <form className="form-detail" method="POST" onSubmit={onsubmit}>
+          <h2>Signup for a new account</h2>
           {Object.keys(errors).length > 0 && (<div className="alert alert-danger">
             {Object.keys(errors).map(key => (errors[key].map((error, index) => (
               <p key={`${key}-${index}`}> {error}</p>))))}
           </div>)}
-          <div className="card-body">
-            <form method="POST" onSubmit={onsubmit}>
-              <div className="row mb-3">
-                <label htmlFor="name" className="col-md-4 col-form-label text-md-end">Name</label>
-                <div className="col-md-6">
-                  <input ref={nameRef} id="name" type="text"
-                         className="form-control" name="name"
-                         required autoComplete="name" autoFocus/>
-                </div>
-              </div>
-              <div className="row mb-3">
-                <label htmlFor="email"
-                       className="col-md-4 col-form-label text-md-end">Email</label>
-
-                <div className="col-md-6">
-                  <input ref={emailRef} id="email" type="email"
-                         className="form-control" name="email"
-                         required autoComplete="email"/>
-                </div>
-              </div>
-              <div className="row mb-3">
-                <label htmlFor="password"
-                       className="col-md-4 col-form-label text-md-end">Password</label>
-
-                <div className="col-md-6">
-                  <input ref={passwordRef} id="password" type="password"
-                         className="form-control" name="password"
-                         required autoComplete="new-password"/>
-                </div>
-              </div>
-              <div className="row mb-3">
-                <label htmlFor="password-confirm"
-                       className="col-md-4 col-form-label text-md-end">Confirm Password</label>
-
-                <div className="col-md-6">
-                  <input ref={passwordConfirmRef} id="password-confirm" type="password" className="form-control"
-                         name="password_confirmation" required autoComplete="new-password"/>
-                </div>
-              </div>
-              <div className="row mb-3">
-                <label htmlFor="phone" className="col-md-4 col-form-label text-md-end">Phone</label>
-                <div className="col-md-6">
-                  <input ref={phoneRef} id="phone" type="text"
-                         className="form-control"
-                         name="phone" autoComplete="phone"/>
-                </div>
-              </div>
-              <div className="row mb-3">
-                <label className="col-md-4 col-form-label text-md-right">User Type</label>
-                <div className="col-md-6">
-                  <div className="form-check">
-                    <label className="form-check-label" htmlFor="instructor">
-                      <input className="form-check-input" type="radio" name="type" id="instructor"
-                             value="instructor" ref={instructorRef} required/>
-                      Instructor </label>
-                  </div>
-                  <div className="form-check">
-                    <label className="form-check-label" htmlFor="student">
-                      <input className="form-check-input" type="radio" name="type" id="student"
-                             value="student" ref={studentRef}/>
-                      Student
-                    </label>
-                  </div>
-                </div>
-              </div>
-              <div className="row mb-0">
-                <div className="col-md-6 offset-md-4">
-                  <button type="submit" className="btn btn-primary">
-                    Signup
-                  </button>
-                  <p className="mt-3">
-                    Already have an account? <Link to="/login">Login</Link>
-                  </p>
-                </div>
-              </div>
-            </form>
+          <div className="form-row">
+            <input ref={nameRef} id="name" type="text"
+                   className="input-text" name="name"
+                   required autoComplete="name" autoFocus placeholder="Your Name"/>
           </div>
-        </div>
+          <div className="form-row">
+            <input ref={emailRef} id="email" type="email"
+                   className="input-text" name="email"
+                   required autoComplete="email" placeholder="Enter a valid Email"/>
+          </div>
+          <div className="form-row">
+            <input ref={passwordRef} id="password" type="password"
+                   className="input-text" name="password"
+                   required autoComplete="new-password" placeholder="Enter a password"/>
+          </div>
+          <div className="form-row">
+            <input ref={passwordConfirmRef} id="password-confirm" type="password" className="input-text"
+                   name="password_confirmation" required autoComplete="new-password" placeholder="Confirm Password"/>
+          </div>
+          <div className="form-row">
+            <input ref={phoneRef} id="phone" type="text"
+                   className="input-text"
+                   name="phone" autoComplete="phone" placeholder="Enter your Phone Number (Optional)"/>
+          </div>
+          <div className="form-row">
+            <label className="user-type">User Type</label>
+            <div className="user-type-options">
+              <label htmlFor="instructor">
+                <input type="radio" name="type" id="instructor"
+                       value="instructor" ref={instructorRef} required/>
+                Instructor </label>
+              <label htmlFor="student">
+                <input type="radio" name="type" id="student"
+                       value="student" ref={studentRef}/>
+                Student
+              </label>
+            </div>
+          </div>
+          <div className="form-row-last">
+            <button type="submit" className="register">
+              Signup
+            </button>
+            <p className="mt-3">
+              Already have an account? <Link to="/login">Login</Link>
+            </p>
+          </div>
+        </form>
       </div>
     </div>
-  </div>)
+  )
 }
