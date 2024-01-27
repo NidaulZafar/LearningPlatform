46eb1d8c969649127912cafedaa2e67a5017928d
N Zafar <n_zafar_007@hotmail.com>
2024-01-27 19:04:13 +0100

    not found page

--- react/src/views/NotFound.jsx
+++ react/src/views/NotFound.jsx
@@ -1,11 +1,11 @@
+import Footer from "../components/Footer.jsx";
+
 export default function NotFound() {
-  return (
-    <div>
-      404 - Page Not Found
-      Are you Lost?
-      Let's go back to the <a href="/">Home Page</a>
-      Or maybe you want to <a href="/login">Login</a>?
-      New user? <a href="/register">Register</a> here.
-    </div>
-  )
+  return (<div>
+    <h1>404 - Page Not Found</h1>
+    Are you Lost?
+    Let's go back to the <a href="/">Home Page</a><br/>
+    Or maybe you want to <a href="/login">Login</a>?<br/>
+    New user? <a href="/signup">Sign up</a> here.
+  </div>)
 }
