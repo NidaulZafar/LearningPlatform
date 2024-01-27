646f0a5c98a6d33bb08d3ddb7bc54b0a7ce427fb
N Zafar <n_zafar_007@hotmail.com>
2024-01-27 20:05:54 +0100

    instructor profile updated

--- react/src/components/InstructorProfile.jsx
+++ react/src/components/InstructorProfile.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import axiosClient from "../axios-client.js";
+import Sidebar from "./Sidebar.jsx";
 
 const InstructorProfile = () => {
   const [instructorData, setInstructorData] = useState({
@@ -13,31 +14,43 @@ const InstructorProfile = () => {
   });
 
   useEffect(() => {
-    // Fetch instructor data from the server based on the user's ID or other identifier
     axiosClient.get("/instructor-profile").then((response) => {
       setInstructorData(response.data);
     });
-  }, []); // Empty dependency array ensures the effect runs only once after the initial render
+  }, []);
 
   return (
-    <div>
-      <h3>Instructor Profile</h3>
-      <div className="row">
-        <div className="col-md-4">
-          <img
-            src={instructorData.avatar || "default-avatar.jpg"}
-            alt={`${instructorData.name}'s Avatar`}
-            className="img-fluid rounded-circle"
-          />
-        </div>
-        <div className="col-md-8">
-          <h4>{instructorData.name}</h4>
-          <p>Email: {instructorData.email}</p>
-          <p>Bio: {instructorData.bio  || 'Not Provided'}</p>
-          <p>Education: {instructorData.education || 'Not Provided'}</p>
-          <p>Occupation: {instructorData.occupation || 'Not Provided'}</p>
-          <p>Phone: {instructorData.phone || 'Not Provided'}</p>
-        </div>
+    <div className="layout has-sidebar fixed-sidebar fixed-header">
+      <Sidebar/>
+      <div id="overlay" className="overlay"></div>
+      <div className="layout">
+        <main className="content">
+          <div>
+            <a id="btn-toggle" href="#" className="sidebar-toggler break-point-sm">
+              <i className="ri-menu-line ri-xl"></i>
+            </a>
+            <h1 style={{marginBottom: '0'}}>Instructor Profile</h1>
+            <span style={{display: 'inline-block'}}>
+              Here you can see your instructor profile.
+            </span>
+            <br/>
+            <img
+              src={instructorData.avatar || "default-avatar.jpg"}
+              alt={`${instructorData.name}'s Avatar`}
+              className="img-fluid rounded-circle"
+            />
+          </div>
+          <div>
+            <h2>Dashboard</h2>
+            <ul>
+              <li><strong>Name:</strong> {instructorData.name}</li>
+              <li><strong>Email:</strong> {instructorData.email}</li>
+              <li><strong>Education:</strong> {instructorData.education || 'Not provided'}</li>
+              <li><strong>Phone:</strong> {instructorData.phone || 'Not provided'}</li>
+              <li><strong>Bio:</strong> {instructorData.bio || 'Not provided'}</li>
+            </ul>
+          </div>
+        </main>
       </div>
     </div>
   );
