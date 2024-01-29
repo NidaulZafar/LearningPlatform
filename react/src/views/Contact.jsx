57ad0b37b791b4e906beb5b358fbdb925fdb3645
N Zafar <n_zafar_007@hotmail.com>
2024-01-29 18:06:57 +0100

    Contact refactor

--- react/src/views/Contact.jsx
+++ react/src/views/Contact.jsx
@@ -1,13 +1,13 @@
-export default function Contact(){
-return(
-<>
-How would you like to contact us?
-<ul>
-<li>Phone</li>
-<li>Email</li>
-<li>Chat</li>
-</ul>
+export default function Contact() {
+  return (
+    <>
+      How would you like to contact us?
+      <ul>
+        <li>Phone</li>
+        <li>Email</li>
+        <li>Chat</li>
+      </ul>
 
-</>
-)
+    </>
+  )
 }
