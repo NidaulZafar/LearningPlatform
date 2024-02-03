b831f9cf379b929c771a786235ae85817a9f589a
N Zafar <n_zafar_007@hotmail.com>
2024-02-03 14:31:20 +0100

    announcements refactored

--- react/src/views/Announcements.jsx
+++ react/src/views/Announcements.jsx
@@ -1,9 +1,7 @@
 import Sidebar from "../components/Sidebar.jsx";
 import React from "react";
-import './announcements.css';
 
 export default function Announcements() {
-//   import announcements from announcements table
   const announcements = [
     {id: 1, title: "Announcement 1", body: "This is the first announcement"},
     {id: 2, title: "Announcement 2", body: "This is the second announcement"},
@@ -12,25 +10,25 @@ export default function Announcements() {
   return (
     <>
       <Sidebar/>
-        <main className="content">
-          <div>
-            <h1 style={{marginBottom: '0'}}>Announcements</h1>
-            <span style={{display: 'inline-block'}}>
+      <main className="content">
+        <div>
+          <h1 style={{marginBottom: '0'}}>Announcements</h1>
+          <span style={{display: 'inline-block'}}>
               Check the latest announcements here.
             </span>
-            <br/>
-          </div>
-          <ul>
-            {announcements.map((announcement) => {
-              return (
-                <li key={announcement.id}>
-                  <h3>{announcement.title}</h3>
-                  <p>{announcement.body}</p>
-                </li>
-              )
-            })}
-          </ul>
-        </main>
+          <br/>
+        </div>
+        <ul>
+          {announcements.map((announcement) => {
+            return (
+              <li key={announcement.id}>
+                <h3>{announcement.title}</h3>
+                <p>{announcement.body}</p>
+              </li>
+            )
+          })}
+        </ul>
+      </main>
     </>
   )
 }
