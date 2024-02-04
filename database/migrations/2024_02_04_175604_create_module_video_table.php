940767341c12cde7c799aff1973fd62e8dd21feb
N Zafar <n_zafar_007@hotmail.com>
2024-02-05 00:24:01 +0100

    module_video pivot table

--- database/migrations/2024_02_04_175604_create_module_video_table.php
+++ database/migrations/2024_02_04_175604_create_module_video_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('video_module', static function (Blueprint $table) {
+        Schema::create('module_video', static function (Blueprint $table) {
             $table->id();
             $table->foreignId('video_id')->constrained();
             $table->foreignId('module_id')->constrained();
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('video_module');
+        Schema::dropIfExists('module_video');
     }
 };
