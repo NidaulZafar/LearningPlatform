e92fb93cf15489a97484fe582ddf2bad1babeceb
N Zafar <n_zafar_007@hotmail.com>
2024-02-06 19:38:41 +0100

    Enrollment model updated

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -5,12 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
 class Enrollment extends Model
 {
     use HasFactory;
 
-    // Enrollment model
+    protected $fillable = [
+        'course_id',
+        'student_id',
+        'status',
+        'enrolled_at',
+    ];
+
     public function student(): BelongsTo
     {
         return $this->belongsTo(Student::class);
@@ -18,7 +25,7 @@ class Enrollment extends Model
 
     public function course(): BelongsTo
     {
-        return $this->belongsTo(Course::class);
+        return $this->belongsTo(Course::class, 'course_id', 'id');
     }
 
 }
