457d8fb44dd6c98555b906303506dd7975e59c78
N Zafar <n_zafar_007@hotmail.com>
2024-02-09 17:37:40 +0100

    feedback model created

---
+++ app/Models/Feedback.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Feedback extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'student_id',
+        'instructor_id',
+        'title',
+        'email',
+        'message',
+    ];
+
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function instructor(): BelongsTo
+    {
+        return $this->belongsTo(Instructor::class);
+    }
+
+}
