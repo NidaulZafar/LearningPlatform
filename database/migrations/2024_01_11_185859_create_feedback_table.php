9cf08b0d1a54473478970bacc482e42d3eb708e3
N Zafar <n_zafar_007@hotmail.com>
2024-02-09 17:36:05 +0100

    Feedback migration updated

--- database/migrations/2024_01_11_185859_create_feedback_table.php
+++ database/migrations/2024_01_11_185859_create_feedback_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('feedback', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('student_id')->nullable()->constrained('students')->onDelete('cascade');
+            $table->foreignId('instructor_id')->nullable()->constrained('instructors')->onDelete('cascade');
+            $table->string('title');
+            $table->string('email');
+            $table->text('message');
             $table->timestamps();
         });
     }
