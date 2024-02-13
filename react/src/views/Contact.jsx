d6a1ec297a6f00864ec77d3b3b02aa39e4ca8f84
N Zafar <n_zafar_007@hotmail.com>
2024-02-13 22:23:59 +0100

    Contact page update

--- react/src/views/Contact.jsx
+++ react/src/views/Contact.jsx
@@ -1,6 +1,9 @@
+import Sidebar from "../components/Sidebar.jsx";
+
 export default function Contact() {
   return (
     <>
+      <Sidebar/>
       How would you like to contact us?
       <ul>
         <li>Phone</li>
