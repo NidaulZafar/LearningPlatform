3fa757dc66c97d186a9bc0c72b8ccb6ff99e5779
N Zafar <n_zafar_007@hotmail.com>
2024-02-21 11:25:46 +0100

    module controller added

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Module;
 use Illuminate\Http\Request;
 
 class ModuleController extends Controller
 {
-    //
+    public function getModuleContent($moduleId){
+        $module = Module::findOrfail($moduleId);
+        return response()->json($module);
+    }
 }
