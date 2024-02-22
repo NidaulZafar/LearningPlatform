55819a25ebcd7df497f97c97c2d657997c1c8b8a
N Zafar <n_zafar_007@hotmail.com>
2024-02-22 01:07:22 +0100

    module controller updated

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Module;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ModuleController extends Controller
 {
-    public function getModuleContent($moduleId){
-        $module = Module::findOrfail($moduleId);
+    public function getModuleContent($moduleId): JsonResponse
+    {
+        $module = Module::with('videos')->findOrfail($moduleId);
         return response()->json($module);
     }
+
 }
