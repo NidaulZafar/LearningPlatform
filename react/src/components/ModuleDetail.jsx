77f342e4c0f5f73e3343141d06838b974653e262
N Zafar <n_zafar_007@hotmail.com>
2024-02-23 15:45:58 +0100

    module detail refactor

--- react/src/components/ModuleDetail.jsx
+++ react/src/components/ModuleDetail.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import axiosClient from '../axios-client';
 import {useParams} from "react-router-dom";
 import Sidebar from "./Sidebar.jsx";
-import './module.css';
+import './styles/module.css';
 
 const ModuleDetail = () => {
   const {id} = useParams();
