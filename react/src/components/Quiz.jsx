b65f66ad7aa23b02b2fdb9dbacf401d45069952a
N Zafar <n_zafar_007@hotmail.com>
2024-02-23 15:59:14 +0100

    assignments paged added in router

---
+++ react/src/components/Quiz.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./Sidebar.jsx";
+
+const Quiz = () => {
+    return (
+      <>
+        <Sidebar />
+        <main className="content">
+Quiz page coming soon!        </main>
+      </>
+    );
+};
+
+export default Quiz;
