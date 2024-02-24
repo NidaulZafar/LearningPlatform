ec89bebf4d57604d52778b84f1a6d3f7dcd70a41
N Zafar <n_zafar_007@hotmail.com>
2024-02-24 15:30:11 +0100

    new course component updated

---
+++ react/src/components/NewCourse.jsx
@@ -0,0 +1,20 @@
+import Sidebar from "./Sidebar.jsx";
+
+const NewCourse = () => {
+  return (
+    <>
+      <Sidebar/>
+      <main className="content">
+      <h1>New Course Form</h1>
+        <p>
+          Here you can add a new course.
+        </p>
+        <form>
+
+        </form>
+      </main>
+    </>
+  );
+};
+
+export default NewCourse;
