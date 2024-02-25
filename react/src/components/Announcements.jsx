199a15bf3391093f10c36b13dead09786ef03230
N Zafar <n_zafar_007@hotmail.com>
2024-02-25 18:44:30 +0100

    Announcements component

---
+++ react/src/components/Announcements.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "./Sidebar.jsx";
+
+const Announcements = () => {
+  return (<>
+    <Sidebar />
+    <main className="content">
+      <h1>Announcements</h1>
+      <p>There are no announcements at this time.</p>
+    </main>
+  </>);
+}
