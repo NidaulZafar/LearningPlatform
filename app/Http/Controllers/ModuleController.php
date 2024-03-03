d87002a5dfd442557bd115cbd0c9a15712f67fa8
N Zafar <n_zafar_007@hotmail.com>
2024-03-03 19:36:16 +0100

    module detail updated

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -5,13 +5,27 @@ namespace App\Http\Controllers;
 use App\Models\Module;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class ModuleController extends Controller
 {
     public function getModuleContent($moduleId): JsonResponse
     {
-        $module = Module::with('videos')->findOrfail($moduleId);
+        $studentId = Auth::id();
+        $module = Module::with(['videos'])->findOrFail($moduleId);
         return response()->json($module);
+    }
+
+    public function markAsCompleted($moduleId): JsonResponse
+    {
+        Log::info('Marking module as completed');
+        $studentId = auth()->user()->id;
+        Log::info('Student ID: ' . $studentId);
+        DB::table('student_module')->where('student_id', $studentId)->where('module_id', $moduleId)->update(['status'
+        => 'completed']);
+        return response()->json(['message' => 'Module marked as completed']);
     }
 
 }
