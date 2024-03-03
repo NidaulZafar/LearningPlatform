3087086b231a9e2c78f76f3a40d55299f724b867
N Zafar <n_zafar_007@hotmail.com>
2024-03-03 01:38:18 +0100

    update backend to serve frontend

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
 Route::get('/', function () {
-    return view('welcome');
+    return file_get_contents(public_path('react/index.html'));
 });
