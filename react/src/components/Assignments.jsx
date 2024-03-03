5477967a4fe99e35fc712a3f9bc527737cac660a
N Zafar <n_zafar_007@hotmail.com>
2024-03-03 21:58:19 +0100

    assignments updated

--- react/src/components/Assignments.jsx
+++ react/src/components/Assignments.jsx
@@ -2,11 +2,11 @@ import Sidebar from "./Sidebar.jsx";
 
 const Assignment = () => {
   return (<>
-      <Sidebar/>
-      <main className={"content"}>
-        Assignments Page coming soon
-      </main>
-    </>)
+    <Sidebar/>
+    <main className={"content"}>
+      Assignments Page coming soon
+    </main>
+  </>)
 }
 
 export default Assignment;
