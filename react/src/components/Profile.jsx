3b82766fe1235845f9efeeaa53ee4be83c7b7c93
N Zafar <n_zafar_007@hotmail.com>
2024-03-09 17:33:54 +0100

    profile page refactored

--- react/src/components/Profile.jsx
+++ react/src/components/Profile.jsx
@@ -9,7 +9,6 @@ const Profile = () => {
   const [editing, setEditing] = useState(false);
   const [editedUser, setEditedUser] = useState(user);
 
-
   useEffect(() => {
     setEditedUser(user);
   }, [user]);
@@ -37,53 +36,53 @@ const Profile = () => {
   const {name, type, email, phone, bio, occupation, education} = user;
 
   const renderProfileHeader = () => (<div className="profile-header">
-      <div className="profile-info">
-        <h1>
-          {editing ? (
-            <input type="text" name="name" value={editedUser.name || ''} onChange={handleInputChange}/>) : (name)}
-        </h1>
-        <h3>({type} Profile)</h3>
-      </div>
-      {!editing && <button onClick={() => setEditing(true)}>Edit Profile</button>}
-      {editing && <button onClick={handleSaveChanges}>Save Changes</button>}
-    </div>);
+    <div className="profile-info">
+      <h1>
+        {editing ? (
+          <input type="text" name="name" value={editedUser.name || ''} onChange={handleInputChange}/>) : (name)}
+      </h1>
+      <h3>({type} Profile)</h3>
+    </div>
+    {!editing && <button onClick={() => setEditing(true)}>Edit Profile</button>}
+    {editing && <button onClick={handleSaveChanges}>Save Changes</button>}
+  </div>);
 
   const renderProfileDetails = () => (<div className="profile-details">
-      <h2>About Me</h2>
-      <p>{editing ? <textarea name="bio" value={editedUser.bio || ''}
-                              onChange={handleInputChange}/> : bio || "Bio not provided"}</p>
-      <h2>Contact Information</h2>
-      <ul>
-        <li>
-          <strong>Email:</strong> {email}
-        </li>
-        <li>
-          <strong>Phone:</strong>{" "}
-          {editing ? (<input type="text" name="phone" value={editedUser.phone || ''}
-                             onChange={handleInputChange}/>) : (phone || "Phone number not provided")}
-        </li>
-      </ul>
-      {type === "instructor" && (<>
-          <h2>Occupation</h2>
-          <p>
-            {editing ? (<input type="text" name="occupation" value={editedUser.occupation || ''}
-                               onChange={handleInputChange}/>) : (occupation || 'Occupation Not provided')}
-          </p>
-        </>)}
-      <h2>Education</h2>
+    <h2>About Me</h2>
+    <p>{editing ? <textarea name="bio" value={editedUser.bio || ''}
+                            onChange={handleInputChange}/> : bio || "Bio not provided"}</p>
+    <h2>Contact Information</h2>
+    <ul>
+      <li>
+        <strong>Email:</strong> {email}
+      </li>
+      <li>
+        <strong>Phone:</strong>{" "}
+        {editing ? (<input type="text" name="phone" value={editedUser.phone || ''}
+                           onChange={handleInputChange}/>) : (phone || "Phone number not provided")}
+      </li>
+    </ul>
+    {type === "instructor" && (<>
+      <h2>Occupation</h2>
       <p>
-        {editing ? (<input type="text" name="education" value={editedUser.education || ''}
-                           onChange={handleInputChange}/>) : (education || "Education not provided")}
+        {editing ? (<input type="text" name="occupation" value={editedUser.occupation || ''}
+                           onChange={handleInputChange}/>) : (occupation || 'Occupation Not provided')}
       </p>
-    </div>);
+    </>)}
+    <h2>Education</h2>
+    <p>
+      {editing ? (<input type="text" name="education" value={editedUser.education || ''}
+                         onChange={handleInputChange}/>) : (education || "Education not provided")}
+    </p>
+  </div>);
 
   return (<>
-      <Sidebar/>
-      <main className="content">
-        {renderProfileHeader()}
-        {renderProfileDetails()}
-      </main>
-    </>);
+    <Sidebar/>
+    <main className="content">
+      {renderProfileHeader()}
+      {renderProfileDetails()}
+    </main>
+  </>);
 };
 
 export default Profile;
