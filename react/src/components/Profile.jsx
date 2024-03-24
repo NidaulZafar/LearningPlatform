673c0ff709f8d124f8b3d3d55992102771505d65
N Zafar <n_zafar_007@hotmail.com>
2024-03-24 17:51:43 +0100

    profile page refactored

--- react/src/components/Profile.jsx
+++ react/src/components/Profile.jsx
@@ -49,36 +49,36 @@ const Profile = () => {
 
   const renderProfileDetails = () => (<div className="profile-details">
     <div className="profile-section">
-    <h2>About Me</h2>
-    <p>{editing ? <textarea name="bio" value={editedUser.bio || ''}
-                            onChange={handleInputChange}/> : bio || "Bio not provided"}</p>
+      <h2>About Me</h2>
+      <p>{editing ? <textarea name="bio" value={editedUser.bio || ''}
+                              onChange={handleInputChange}/> : bio || "Bio not provided"}</p>
     </div>
     <div className="profile-section">
-    <h2>Contact Information</h2>
-    <ul>
-      <li>
-        <strong>Email:</strong> {email}
-      </li>
-      <li>
-        <strong>Phone:</strong>{" "}
-        {editing ? (<input type="text" name="phone" value={editedUser.phone || ''}
-                           onChange={handleInputChange}/>) : (phone || "Phone number not provided")}
-      </li>
-    </ul>
+      <h2>Contact Information</h2>
+      <ul>
+        <li>
+          <strong>Email:</strong> {email}
+        </li>
+        <li>
+          <strong>Phone:</strong>{" "}
+          {editing ? (<input type="text" name="phone" value={editedUser.phone || ''}
+                             onChange={handleInputChange}/>) : (phone || "Phone number not provided")}
+        </li>
+      </ul>
     </div>
     <div className="profile-section">
-    {type === "instructor" && (<>
-      <h2>Occupation</h2>
+      {type === "instructor" && (<>
+        <h2>Occupation</h2>
+        <p>
+          {editing ? (<input type="text" name="occupation" value={editedUser.occupation || ''}
+                             onChange={handleInputChange}/>) : (occupation || 'Occupation Not provided')}
+        </p>
+      </>)}
+      <h2>Education</h2>
       <p>
-        {editing ? (<input type="text" name="occupation" value={editedUser.occupation || ''}
-                           onChange={handleInputChange}/>) : (occupation || 'Occupation Not provided')}
+        {editing ? (<input type="text" name="education" value={editedUser.education || ''}
+                           onChange={handleInputChange}/>) : (education || "Education not provided")}
       </p>
-    </>)}
-    <h2>Education</h2>
-    <p>
-      {editing ? (<input type="text" name="education" value={editedUser.education || ''}
-                         onChange={handleInputChange}/>) : (education || "Education not provided")}
-    </p>
     </div>
   </div>);
 
