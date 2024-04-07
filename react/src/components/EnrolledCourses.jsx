964dc68b1b12f89ca564b77e6bb9b4beb0f8f29e
N Zafar <n_zafar_007@hotmail.com>
2024-04-07 13:26:58 +0200

    enrolled courses page refactored

--- react/src/components/EnrolledCourses.jsx
+++ react/src/components/EnrolledCourses.jsx
@@ -1,61 +1,74 @@
-import {useStateContext} from "../contexts/ContextProvider.jsx";
-import {useEffect, useState} from "react";
+import { useStateContext } from "../contexts/ContextProvider.jsx";
+import { useEffect, useState } from "react";
 import axiosClient from "../axios-client.js";
 import Sidebar from "./Sidebar.jsx";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function EnrolledCourses() {
-  const {user} = useStateContext();
+  const { user } = useStateContext();
   const [enrolledCourses, setEnrolledCourses] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
 
-
   useEffect(() => {
-    fetchEnrolledCourses().then(r => r).catch(e => e);
+    fetchEnrolledCourses()
+      .then((r) => r)
+      .catch((e) => e);
   }, []);
 
   const fetchEnrolledCourses = async () => {
     try {
-      const response = await axiosClient.get('/enrolled-courses');
+      const response = await axiosClient.get("/enrolled-courses");
       if (Array.isArray(response.data)) {
         setEnrolledCourses(response.data);
       } else {
         setErrorMessage(response.data.message);
       }
     } catch (error) {
-      console.error('Error fetching enrolled courses', error);
+      console.error("Error fetching enrolled courses", error);
     }
   };
 
   function formatDate(dateString) {
-    const options = {year: 'numeric', month: 'long', day: 'numeric'};
+    const options = { year: "numeric", month: "long", day: "numeric" };
     return new Date(dateString).toLocaleDateString(undefined, options);
   }
 
-
-  return (<>
-    <Sidebar/>
-    <main className="content">
-      <h1>Enrolled Courses</h1>
-      <div className="course-list">
-        <p>Welcome, {user.name}!</p>
-        {errorMessage ? (<p>{errorMessage}</p>) : (<>
-          <p>Here are the courses you have already enrolled:</p>
-          <div className="course-grid">
-            {enrolledCourses.map((enrollment) => (
-              <Link to={`/courses/${enrollment.course.id}`} key={enrollment.course.id}>
-                <div key={enrollment.course.id} className="course-card">
-                  <img className="course-image"
-                       src={enrollment.course.cover_image} alt={enrollment.course.title}/>
-                  <div className="course-details">
-                    <h3>{enrollment.course.title}</h3>
-                    <p>Enrolled on: {formatDate(enrollment.created_at)}</p>
-                  </div>
-                </div>
-              </Link>))}
-          </div>
-        </>)}
-      </div>
-    </main>
-  </>)
+  return (
+    <>
+      <Sidebar />
+      <main className="content">
+        <h1>Enrolled Courses</h1>
+        <div className="course-list">
+          <p>Welcome, {user.name}!</p>
+          {errorMessage ? (
+            <p>{errorMessage}</p>
+          ) : (
+            <>
+              <p>Here are the courses you have already enrolled:</p>
+              <div className="course-grid">
+                {enrolledCourses.map((enrollment) => (
+                  <Link
+                    to={`/courses/${enrollment.course.id}`}
+                    key={enrollment.course.id}
+                  >
+                    <div key={enrollment.course.id} className="course-card">
+                      <img
+                        className="course-image"
+                        src={enrollment.course.cover_image}
+                        alt={enrollment.course.title}
+                      />
+                      <div className="course-details">
+                        <h3>{enrollment.course.title}</h3>
+                        <p>Enrolled on: {formatDate(enrollment.created_at)}</p>
+                      </div>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+      </main>
+    </>
+  );
 }
