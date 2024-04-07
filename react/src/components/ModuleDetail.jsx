b76f308ae7fb14a811ec9fe83d144dd83c10cb82
N Zafar <n_zafar_007@hotmail.com>
2024-04-07 13:28:27 +0200

    module details refactored

--- react/src/components/ModuleDetail.jsx
+++ react/src/components/ModuleDetail.jsx
@@ -1,11 +1,11 @@
-import React, {useEffect, useState} from 'react';
-import axiosClient from '../axios-client';
-import {Link, useParams} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import axiosClient from "../axios-client";
+import { Link, useParams } from "react-router-dom";
 import Sidebar from "./Sidebar.jsx";
-import './styles/module.css';
+import "./styles/module.css";
 
 const ModuleDetail = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const [moduleData, setModuleData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -18,8 +18,8 @@ const ModuleDetail = () => {
         setModuleData(response.data);
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching module data:', error);
-        setError('Error fetching module data');
+        console.error("Error fetching module data:", error);
+        setError("Error fetching module data");
         setLoading(false);
       }
     };
@@ -34,11 +34,13 @@ const ModuleDetail = () => {
       setTimeout(() => {
         setMessage(null);
       }, 3000);
-      setModuleData(prevModuleData => ({...prevModuleData, status: 'completed'}));
+      setModuleData((prevModuleData) => ({
+        ...prevModuleData,
+        status: "completed",
+      }));
     } catch (error) {
-      console.error('Error marking module as completed:', error);
+      console.error("Error marking module as completed:", error);
     }
-    ;
   };
 
   if (loading) {
@@ -49,49 +51,67 @@ const ModuleDetail = () => {
     return <div>Error: {error}</div>;
   }
 
-  const {title, description, content, resource_files, resource_links, duration, videos, course_id} = moduleData;
+  const {
+    title,
+    description,
+    content,
+    resource_files,
+    resource_links,
+    duration,
+    videos,
+    course_id,
+  } = moduleData;
 
-  return (<>
-    <Sidebar/>
-    <main className="content">
-      <div className="module-detail">
-        <h2>{title}</h2>
-        <p>Status: {moduleData.status}</p>
-        <p>Description: {description}</p>
-        <p>Content: {content}</p>
-        <p>Duration: {duration} minutes</p>
-        <div>
-          <h3>Videos:</h3>
-          <ul>
-            {videos.map(video => (<li key={video.id}>
-              <h4>{video.title}</h4>
-              <p>Description: {video.description}</p>
-              <p>Duration: {video.duration} minutes</p>
-              <Link href={video.video_url} target="_blank" rel="noopener noreferrer">
-                <img src={video.thumbnail}
-                     alt={video.title}/>
-                <br/>
-                Watch Video</Link>
-            </li>))}
-          </ul>
+  return (
+    <>
+      <Sidebar />
+      <main className="content">
+        <div className="module-detail">
+          <h2>{title}</h2>
+          <p>Status: {moduleData.status}</p>
+          <p>Description: {description}</p>
+          <p>Content: {content}</p>
+          <p>Duration: {duration} minutes</p>
+          <div>
+            <h3>Videos:</h3>
+            <ul>
+              {videos.map((video) => (
+                <li key={video.id}>
+                  <h4>{video.title}</h4>
+                  <p>Description: {video.description}</p>
+                  <p>Duration: {video.duration} minutes</p>
+                  <Link
+                    href={video.video_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={video.thumbnail} alt={video.title} />
+                    <br />
+                    Watch Video
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <h4>Extra Resources:</h4>
+          <p>Resource Files: {resource_files}</p>
+          <p>Resource Links: {resource_links}</p>
+          {moduleData.status === "completed" ? (
+            <p>Module Completed!</p>
+          ) : (
+            <button onClick={markAsCompleted}>Mark as Completed</button>
+          )}
+          <Link to={`/courses/${course_id}`}>
+            <button>Back to Course</button>
+          </Link>
+          <Link to={`/courses`}>
+            <button>All Courses</button>
+          </Link>
         </div>
-        <h4>Extra Resources:</h4>
-        <p>Resource Files: {resource_files}</p>
-        <p>Resource Links: {resource_links}</p>
-        {moduleData.status === 'completed' ? <p>Module Completed!</p> :
-          <button onClick={markAsCompleted}>Mark as Completed</button>}
-        <Link to={`/courses/${course_id}`}>
-          <button>Back to Course</button>
-        </Link>
-        <Link to={`/courses`}>
-          <button>All Courses</button>
-        </Link>
-      </div>
-      {message && (<div className="message">
-        {message}
-      </div>)}
-    </main>
-  </>);
+        {message && <div className="message">{message}</div>}
+      </main>
+    </>
+  );
 };
 
 export default ModuleDetail;
