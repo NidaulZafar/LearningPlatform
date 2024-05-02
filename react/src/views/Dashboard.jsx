b98a058deec7068465bc166bad7c0ba9168130e9
N Zafar <n_zafar_007@hotmail.com>
2024-05-02 21:22:12 +0200

    white page error debugging

--- react/src/views/Dashboard.jsx
+++ react/src/views/Dashboard.jsx
@@ -1,21 +1,17 @@
-import {useStateContext} from "../contexts/ContextProvider.jsx";
-import {useNavigate} from "react-router-dom";
-import {useEffect} from "react";
+import { useStateContext } from "../contexts/ContextProvider.jsx";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import Profile from "../components/Profile.jsx";
 
 export default function Dashboard() {
   const navigate = useNavigate();
-  const {user, setUser, setToken} = useStateContext();
+  const { user, setUser, setToken } = useStateContext();
 
   useEffect(() => {
     if (!user) {
-      navigate('/login');
+      navigate("/login");
     }
   }, [user, navigate]);
 
-  return (
-    <>
-      {user && user.type && <Profile />}
-    </>
-  )
+  return <>{user && user.type && <Profile />}</>;
 }
