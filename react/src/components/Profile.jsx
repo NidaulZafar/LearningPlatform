0d9d2d73a99046190ee92a52fc9de5c70d727a12
N Zafar <n_zafar_007@hotmail.com>
2024-05-05 16:26:37 +0200

    profile page refactored to extract profile details

--- react/src/components/Profile.jsx
+++ react/src/components/Profile.jsx
@@ -3,6 +3,7 @@ import axiosClient from "../axios-client.js";
 import Sidebar from "./Sidebar.jsx";
 import "./styles/profile.css";
 import { useStateContext } from "../contexts/ContextProvider.jsx";
+import {renderProfileDetails, renderProfileHeader} from "./ProfileUtils.jsx";
 
 const Profile = () => {
   const { user, setUser } = useStateContext();
@@ -42,110 +43,12 @@ const Profile = () => {
     }
   };
 
-  const { name, type, email, phone, bio, occupation, education } = user;
-
-  const renderProfileHeader = () => (
-    <div className="profile-header">
-      <div className="profile-info">
-        <h1>
-          {editing ? (
-            <input
-              type="text"
-              name="name"
-              value={editedUser.name || ""}
-              onChange={handleInputChange}
-            />
-          ) : (
-            name
-          )}
-        </h1>
-        <h3>({type})</h3>
-      </div>
-      {!editing && (
-        <button onClick={() => setEditing(true)}>Edit Profile</button>
-      )}
-      {editing && <button onClick={handleSaveChanges}>Save Changes</button>}
-    </div>
-  );
-
-  const renderProfileDetails = () => (
-    <div className="profile-details">
-      <div className="profile-section">
-        <h2>About Me</h2>
-        <p>
-          {editing ? (
-            <textarea
-              name="bio"
-              value={editedUser.bio || ""}
-              onChange={handleInputChange}
-            />
-          ) : (
-            bio || "Bio not provided"
-          )}
-        </p>
-      </div>
-      <div className="profile-section">
-        <h2>Contact Information</h2>
-        <ul>
-          <li>
-            <strong>Email:</strong> {email}
-          </li>
-          <li>
-            <strong>Phone:</strong>{" "}
-            {editing ? (
-              <input
-                type="text"
-                name="phone"
-                value={editedUser.phone || ""}
-                onChange={handleInputChange}
-              />
-            ) : (
-              phone || "Phone number not provided"
-            )}
-          </li>
-        </ul>
-      </div>
-      <div className="profile-section">
-        {type === "instructor" && (
-          <>
-            <h2>Occupation</h2>
-            <p>
-              {editing ? (
-                <input
-                  type="text"
-                  name="occupation"
-                  value={editedUser.occupation || ""}
-                  onChange={handleInputChange}
-                />
-              ) : (
-                occupation || "Occupation Not provided"
-              )}
-            </p>
-          </>
-        )}
-        <h2>Education</h2>
-        <p>
-          {editing ? (
-            <input
-              type="text"
-              name="education"
-              value={editedUser.education || ""}
-              onChange={handleInputChange}
-            />
-          ) : (
-            education || "Education not provided"
-          )}
-        </p>
-      </div>
-    </div>
-  );
-
   return (
     <>
       <Sidebar />
       <main className="content">
-        {renderProfileHeader()}
-        {renderProfileDetails()}
+        {renderProfileHeader(user, editing, setEditing, editedUser, handleInputChange, handleSaveChanges)}
+        {renderProfileDetails(user, editing, editedUser, handleInputChange)}
       </main>
     </>
   );
