3fc4295e3e6e372322ad39fffeb8ae39c8d4aa19
N Zafar <n_zafar_007@hotmail.com>
2024-05-06 00:29:26 +0200

    student module controller

--- app/Http/Controllers/StudentModuleController.php
+++ app/Http/Controllers/StudentModuleController.php
@@ -16,11 +16,8 @@ class StudentModuleController extends Controller
     public function getStudentModules(Request $request): JsonResponse
     {
         $studentId = Auth::id();
-        Log::info('Student ID: ' . $studentId);
         $moduleIds = explode(',', $request->input('module_ids')); // Split comma-separated string into array
-        Log::info('Module IDs: ' . json_encode($moduleIds));
 
-        // Fetch status for each module_id for the given student_id
         $studentModules = StudentModule::whereIn('module_id', $moduleIds)
             ->where('student_id', $studentId)
             ->get(['module_id', 'status']);
