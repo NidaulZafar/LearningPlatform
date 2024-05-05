1b081e7341107e5cd3497163eb81931256a25564
N Zafar <n_zafar_007@hotmail.com>
2024-05-05 21:41:10 +0200

    Default layout refactored

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,18 +1,18 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useStateContext } from "../contexts/ContextProvider.jsx";
-import React, { useEffect } from "react";
+import {Navigate, Outlet} from "react-router-dom";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
+import React, {useEffect} from "react";
 import axiosClient from "../axios-client.js";
 import Footer from "./Footer.jsx";
 
 export default function DefaultLayout() {
-  const { user, token, setUser, setToken } = useStateContext();
+  const {user, token, setUser, setToken} = useStateContext();
 
   if (!token) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/login"/>;
   }
 
   useEffect(() => {
-    axiosClient.get("/user").then(({ data }) => {
+    axiosClient.get("/user").then(({data}) => {
       setUser(data);
     });
   }, []);
@@ -20,9 +20,9 @@ export default function DefaultLayout() {
   return (
     <>
       <main className="main">
-        <Outlet />
+        <Outlet/>
       </main>
-      <Footer />
+      <Footer/>
     </>
   );
 }
